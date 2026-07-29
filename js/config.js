a7af789fdcd0a381e37d5d55572398beda9952bb
ltsMusa <musaadag29@gmail.com>
2026-07-29 09:46:49 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,33 @@
+/**
+ * ==========================================================
+ * Lotus AI - Configuration
+ * ==========================================================
+ */
 
+const config = {
+
+    provider: "gemini",
+
+    providers: {
+
+        gemini: {
+            apiKey: ""
+        },
+
+        groq: {
+            apiKey: ""
+        },
+
+        openai: {
+            apiKey: ""
+        },
+
+        openrouter: {
+            apiKey: ""
+        }
+
+    }
+
+};
+
+export default config;

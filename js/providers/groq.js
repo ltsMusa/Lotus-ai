63c3dbb9b9842b28dae63f1716b3689c0e5a8fee
ltsMusa <musaadag29@gmail.com>
2026-07-29 10:29:23 +0300

    Update groq.js

--- js/providers/groq.js
+++ js/providers/groq.js
@@ -1,8 +1,46 @@
+import config from "js/config.js";
+
 export default class GroqProvider {
 
     async sendMessage(message) {
 
-        return "⚡ Groq henüz bağlanmadı.";
+        try {
+
+            const response = await fetch(
+                "https://api.groq.com/openai/v1/chat/completions",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${config.providers.groq.apiKey}`
+                    },
+                    body: JSON.stringify({
+                        model: "llama-3.3-70b-versatile",
+                        messages: [
+                            {
+                                role: "user",
+                                content: message
+                            }
+                        ]
+                    })
+                }
+            );
+
+            if (!response.ok) {
+                return `Groq Hatası (${response.status})`;
+            }
+
+            const data = await response.json();
+
+            return data.choices[0].message.content;
+
+        } catch (error) {
+
+            console.error(error);
+
+            return "Groq bağlantısı başarısız.";
+
+        }
 
     }
 

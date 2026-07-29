a5d61977d4d20c71dd4ad6556e621442978037aa
ltsMusa <musaadag29@gmail.com>
2026-07-29 10:36:16 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@ const config = {
         },
 
         groq: {
-            apiKey: ""
+            apiKey: "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"
         },
 
         openai: {

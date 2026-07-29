3825332c0b9441eb34c31bc92212038fb9c88e8c
ltsMusa <musaadag29@gmail.com>
2026-07-29 09:47:40 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -22,3 +22,34 @@ export default class APIManager {
     }
 
 }
+import config from "./config.js";
+
+import GeminiProvider from "./providers/gemini.js";
+import GroqProvider from "./providers/groq.js";
+import OpenAIProvider from "./providers/openai.js";
+import OpenRouterProvider from "./providers/openrouter.js";
+
+export default class APIManager {
+
+    constructor() {
+
+        this.providers = {
+
+            gemini: new GeminiProvider(),
+            groq: new GroqProvider(),
+            openai: new OpenAIProvider(),
+            openrouter: new OpenRouterProvider()
+
+        };
+
+    }
+
+    async sendMessage(message) {
+
+        const provider = this.providers[config.provider];
+
+        return await provider.sendMessage(message);
+
+    }
+
+}
